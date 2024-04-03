e2d2a586f531a04293ea6a0b40a0b95762b80640
Nikita <shchukin_nikita02@mail.ru>
2024-04-03 19:05:02 +0300

    //

--- STEAM_DB/SignInWindow.xaml.cs
+++ STEAM_DB/SignInWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace STEAM_DB
 {
@@ -7,9 +8,11 @@ namespace STEAM_DB
     /// </summary>
     public partial class SignInWindow : Window
     {
+        static bool CheckValidUserName = false;
+        static bool CheckValidPassword = false;
         static readonly string[] sep = [" ", ",", "/"];
-        static string userName = null!;
-        static string password = null!;
+        static string userName = string.Empty;
+        static string password = string.Empty;
         static readonly string errorMessage = "Заполните поля!";
 
         public SignInWindow()
@@ -19,37 +22,68 @@ namespace STEAM_DB
 
         private void ButtonSignIn_Click(object sender, RoutedEventArgs e)
         {
-            userName = string.Join("", textBoxUserName.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
-            password = string.Join("", passwordBox.Password.ToString().Split(sep, StringSplitOptions.RemoveEmptyEntries));
-
-            string errorMessage = "Заполните поля!";
-            if (userName != "")
+            if (CheckValidUserName && CheckValidPassword)
             {
-                if (password != "")
+                string passwordHash = Hash.GetHash(password);
+                bool result = Authentication.CheckThePassword(userName, passwordHash);
+                if (result)
                 {
-                    string passwordHash = Hash.GetHash(password);
-                    bool result = Authentication.CheckThePassword(userName, passwordHash);
-                    if (result)
+                    MessageBox.Show("Вы успешно вошли в систему!");
+                    Close();
+                    Authentication.GetUser(userName, passwordHash);
+                    if (userName == "admin")
                     {
-                        MessageBox.Show("Вы успешно вошли в систему!");
-                        Close();
-                        Authentication.GetUser(userName, passwordHash);
-                        if (userName == "admin")
-                        {
-                            AdminWindow adminWindow = new();
-                            adminWindow.ShowDialog();
-                        }
-                        else
-                        {
-                            PersonalAccountWindow window = new();
-                            window.ShowDialog();
-                        }
+                        AdminWindow adminWindow = new();
+                        adminWindow.ShowDialog();
                     }
-                    else MessageBox.Show("Данные не верны!");
+                    else
+                    {
+                        PersonalAccountWindow window = new();
+                        window.ShowDialog();
+                    }
                 }
-                else MessageBox.Show(errorMessage);
+                else MessageBox.Show("Данные не верны!");
             }
-            else MessageBox.Show(errorMessage);
+            else
+            {
+                if (userName == string.Empty)
+                    textBoxUserName.BorderBrush = Brushes.Red;
+                if (password == string.Empty)
+                    passwordBox.BorderBrush = Brushes.Red;
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        private void TextBoxUserName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            userName = string.Join("", textBoxUserName.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
+            if (userName != string.Empty)
+            {
+                textBoxUserName.BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0x4C, 0x4C, 0x72));
+                if (!CheckValidUserName) CheckValidUserName = true;
+            }
+            else
+            {
+                textBoxUserName.BorderBrush = Brushes.Red;
+                MessageBox.Show("Заполните имя!");
+                if (CheckValidUserName) CheckValidUserName = false;
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            password = string.Join("", passwordBox.Password.ToString().Split(sep, StringSplitOptions.RemoveEmptyEntries));
+            if (password != string.Empty)
+            {
+                passwordBox.BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0x4C, 0x4C, 0x72));
+                if (!CheckValidPassword) CheckValidPassword = true;
+            }
+            else
+            {
+                passwordBox.BorderBrush = Brushes.Red;
+                MessageBox.Show("Введите пароль!");
+                if (CheckValidPassword) CheckValidPassword = false;
+            }
         }
     }
 }
--- STEAM_DB/SignUpWindow.xaml.cs
+++ STEAM_DB/SignUpWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace STEAM_DB
 {
@@ -7,10 +8,13 @@ namespace STEAM_DB
     /// </summary>
     public partial class SignUpWindow : Window
     {
+        static bool CheckValidUserName = false;
+        static bool CheckValidEmail = false;
+        static bool CheckValidPassword = false;
         static readonly string[] sep = [" ", ",", "/"];
-        static string userName = null!;
-        static string email = null!;
-        static string password = null!;
+        static string userName = string.Empty;
+        static string email = string.Empty;
+        static string password = string.Empty;
         static readonly string errorMessage = "Заполните поля!";
 
         public SignUpWindow()
@@ -20,53 +24,94 @@ namespace STEAM_DB
 
         private void ButtonSignUp_Click(object sender, RoutedEventArgs e)
         {
-            userName = string.Join("", textBoxUserName.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
-            email = string.Join("", textBoxEmail.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
-            password = string.Join("", passwordBox.Password.ToString().Split(sep, StringSplitOptions.RemoveEmptyEntries));
-
-            if (userName != "")
+            if (CheckValidUserName && CheckValidEmail && CheckValidEmail)
             {
-                if (email != "")
+                bool checkUserName = Authorization.CheckUserName(userName);
+                if (!checkUserName)
                 {
-                    if (password != "")
+                    bool checkEmail = Authorization.CheckEmail(email);
+                    if (!checkEmail)
                     {
-                        bool checkValidEmail = Authorization.ValidEmail(email);
-                        if (checkValidEmail)
-                        {
-                            bool checkUserName = Authorization.CheckUserName(userName);
-                            if (!checkUserName)
-                            {
-                                bool checkEmail = Authorization.CheckEmail(email);
-                                if (!checkEmail)
-                                {
-                                    // поучаем хеш пароля
-                                    string passwordHash = Hash.GetHash(password);
-                                    // сегодняшняя дата
-                                    DateTime date = DateTime.Today;
-                                    // конвертируем в строку вида yyyy-MM-dd
-                                    string dateString = date.ToString("yyyy-MM-dd");
-                                    // передача параметров в метод AddUserInDB класса AuthorizationDB
-                                    Authorization.AddUserInDB(userName, email, passwordHash, dateString);
-                                    MessageBox.Show("Регистрация прошла успешно!");
-                                    // закрываем окно регистрации
-                                    Close();
-                                    // получаем пользователя, под которым входим в систему
-                                    Authentication.GetUser(userName, passwordHash);
-                                    // открываем окно личного кабинета
-                                    PersonalAccountWindow window = new();
-                                    window.ShowDialog();
-                                }
-                                else MessageBox.Show("Данная почта уже существует!");
-                            }
-                            else MessageBox.Show("Имя занято!");
-                        }
-                        else MessageBox.Show("Неверный формат почты!");
+                        // поучаем хеш пароля
+                        string passwordHash = Hash.GetHash(password);
+                        // сегодняшняя дата
+                        DateTime date = DateTime.Today;
+                        // конвертируем в строку вида yyyy-MM-dd
+                        string dateString = date.ToString("yyyy-MM-dd");
+                        // передача параметров в метод AddUserInDB класса AuthorizationDB
+                        Authorization.AddUserInDB(userName, email, passwordHash, dateString);
+                        MessageBox.Show("Регистрация прошла успешно!");
+                        // закрываем окно регистрации
+                        Close();
+                        // получаем пользователя, под которым входим в систему
+                        Authentication.GetUser(userName, passwordHash);
+                        // открываем окно личного кабинета
+                        PersonalAccountWindow window = new();
+                        window.ShowDialog();
                     }
-                    else MessageBox.Show(errorMessage);
+                    else MessageBox.Show("Данная почта уже существует!");
                 }
-                else MessageBox.Show(errorMessage);
+                else MessageBox.Show("Имя занято!");
             }
-            else MessageBox.Show(errorMessage);
+            else
+            {
+                if (userName == string.Empty)
+                    textBoxUserName.BorderBrush = Brushes.Red;
+                if (email == string.Empty)
+                    textBoxEmail.BorderBrush = Brushes.Red;
+                if (password == string.Empty)
+                    passwordBox.BorderBrush = Brushes.Red;
+                MessageBox.Show(errorMessage);
+            }
+        }
+
+        private void TextBoxUserName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            userName = string.Join("", textBoxUserName.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
+            if (userName != string.Empty)
+            {
+                textBoxUserName.BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0x4C, 0x4C, 0x72));
+                if (!CheckValidUserName) CheckValidUserName = true;
+            }
+            else
+            {
+                textBoxUserName.BorderBrush = Brushes.Red;
+                MessageBox.Show("Заполните имя!");
+                if (CheckValidUserName) CheckValidUserName = false;
+            }
+        }
+
+        private void TextBoxEmail_LostFocus(object sender, RoutedEventArgs e)
+        {
+            email = string.Join("", textBoxEmail.Text.Split(sep, StringSplitOptions.RemoveEmptyEntries));
+            bool checkValidEmail = Authorization.ValidEmail(email);
+            if (checkValidEmail)
+            {
+                textBoxEmail.BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0x4C, 0x4C, 0x72));
+                if (!CheckValidEmail) CheckValidEmail = true;
+            }
+            else
+            {
+                textBoxEmail.BorderBrush = Brushes.Red;
+                MessageBox.Show("Неверный формат почты!");
+                if (CheckValidEmail) CheckValidEmail = false;
+            }
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            password = string.Join("", passwordBox.Password.ToString().Split(sep, StringSplitOptions.RemoveEmptyEntries));
+            if (password != string.Empty)
+            {
+                passwordBox.BorderBrush = new SolidColorBrush(Color.FromArgb(0xff, 0x4C, 0x4C, 0x72));
+                if (!CheckValidPassword) CheckValidPassword = true;
+            }
+            else
+            {
+                passwordBox.BorderBrush = Brushes.Red;
+                MessageBox.Show("Введите пароль!");
+                if (CheckValidPassword) CheckValidPassword = false;
+            }
         }
     }
 }
